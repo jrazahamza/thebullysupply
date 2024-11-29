e868846391da73239404310721eaf9896164d09e
Muslim Raza <fairsit.m@gmail.com>
2024-11-29 08:44:02 +0000

    Deleted the twentytwentytwo theme directory and associated files

--- wp-content/plugins/woo-currency/classes/date.php
+++
@@ -1,9 +0,0 @@
-<?php
-class dateWcu {
-	static public function _($time = NULL) {
-		if(is_null($time)) {
-			$time = time();
-		}
-		return date(WCU_DATE_FORMAT_HIS, $time);
-	}
-}
--- wp-content/plugins/woo-currency/classes/installerDbUpdater.php
+++
@@ -1,6 +0,0 @@
-<?php
-class installerDbUpdaterWcu {
-	static public function runUpdate() {
-
-	}
-}
